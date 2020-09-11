a2b32f86af20e02c0fe8e8262a04cf42598ed614
Nathan Yap <nathany@aithinktank.com>
2020-09-10 18:39:22 -0700

    changed one word

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ class Home extends Component {
   render() {
     return (
       <div className="Home">
-        <h1>Welcome to the homepage!</h1>
+        <h1>Welcome to the outreach homepage!!!</h1>
         <p>You don’t need to be logged in to see this page.</p>
       </div>
     );
